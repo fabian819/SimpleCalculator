bc13021f53f666b993f519547a511196e6a12678
fabian <kitonyifabian@gmail.com>
2025-08-04 12:57:14 +0300

    Initial commit

---
+++ Simple_calculator.py
@@ -0,0 +1,27 @@
+
+
+operator = (input("Enter operator(+ - * / %): "))
+
+num1 =float(input("Enter first number: "))
+
+num2 = float(input("Enter second number: "))
+
+
+if operator == '+':
+    print(num1 + num2)
+elif operator == '-':
+    print(num1 - num2)
+elif operator == '*':
+    print(num1 * num2)
+elif operator == '/':
+    if num2 != 0:
+        print(num1 / num2)
+    else:
+        print("Error: Division by zero is not allowed.")
+elif operator == '%':
+    if num2 != 0:
+        print(num1 % num2)
+    else:
+        print("Error: Division by zero is not allowed.")
+
+
